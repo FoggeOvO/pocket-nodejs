efc2327f11db9b966171e95a4d289f70e8fdbb2d
fogge <fogge.bruce@gmail.com>
2023-12-15 18:26:34 +0800

    UserModel more change

--- models/UserModel.js
+++ models/UserModel.js
@@ -14,7 +14,67 @@ let UserSchema = new mongoose.Schema({
         enum: ['admin', 'user'],
         default: 'user',
         required: true
-    }
+    },
+    workcode: {
+        type: String,
+        required: true
+    },
+    lastname: {
+        type: String,
+        required: true
+    },
+    gender: {
+        type: String,
+        required: true
+    },
+    depid: {
+        type: Number,
+        required: true
+    },
+    hiredate: {
+        type: String,
+        required: true
+    },
+    actConfData: {
+        type: String,
+        required: true
+    },
+    costcenter: {
+        type: String,
+        required: true
+    },
+    isCN: {
+        type: Number,
+        required: true
+    },
+    houseAllance: {
+        type: Number,
+        required: true
+    },
+    isConf: {
+        type: Number,
+        required: true
+    },
+    isTech: {
+        type: Number,
+        required: true
+    },
+    mealAllance: {
+        type: Number,
+        required: true
+    },
+    national: {
+        type: String,
+        required: true
+    },
+    remark: {
+        type: String,
+        required: true
+    },
+    salGroup: {
+        type: Number,
+        required: true
+    },
 }); // 创建一个模式对象
 
 let UserModel = mongoose.model('user', UserSchema); // 创建一个模型对象
--- routes/api/hrm/hrm.js
+++ routes/api/hrm/hrm.js
@@ -25,9 +25,53 @@ router.get('/getusers', checkToken, function (req, res, next) {
     });
 });
 
+router.get('/getUserById', checkToken, function (req, res, next) {
+  const {_id} = req.query
+  UserModel.findOne({ "_id": _id }, { lastname: 1, workcode: 1, hiredate: 1, position: 1, level: 1, gender: 1, _id: 1, hiredata: 1, depid: 1, actConfData: 1, costcenter: 1, houseAllance: 1, isCN: 1, isConf: 1, isTech: 1, mealAllance: 1, national: 1, remark: 1, salGroup: 1 }).then((data) => {
+    if (!data) {
+      res.json({
+        code: '3001',
+        msg: '数据为空'
+      });
+    } else {
+      res.json({
+        code: '0000',
+        msg: 'success',
+        data: data
+      });
+    }
+  })
+    .catch((err) => {
+      res.status(500).send('获取数据异常,请稍后再试~', err);
+    });
+});
+
 router.post('/getuserByDepid', checkToken, function (req, res, next) {
   let { depids } = req.body;
   UserModel.find({ "isdel": 0, "depid": { $in: depids } }, { lastname: 1, workcode: 1, hiredate: 1, position: 1, level: 1, gender: 1, _id: 1, hiredata: 1, depid: 1, actConfData: 1, costcenter: 1, houseAllance: 1, isCN: 1, isConf: 1, isTech: 1, mealAllance: 1, national: 1, remark: 1, salGroup: 1 }).then((data) => {
+    if (!data) {
+      res.json({
+        code: '3001',
+        msg: '数据为空'
+      });
+    } else {
+      res.json({
+        code: '0000',
+        msg: 'success',
+        data: data
+      });
+    }
+  })
+    .catch((err) => {
+      res.status(500).send('获取数据异常,请稍后再试~');
+    });
+});
+
+router.put('/updateUserById', checkToken, function (req, res, next) {
+  console.log('@@',req.body)
+  let { _id,...updateData } = req.body;
+  console.log('@',updateData)
+  UserModel.updateOne({ "_id":  _id},{$set:updateData}).then((data) => {
     if (!data) {
       res.json({
         code: '3001',
